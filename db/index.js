63571f286c7a585fdd01b1210c9c413c5c5324b9
Dsmcamila <camisanchez.cursos@gmail.com>
2024-02-24 00:43:47 -0500

    Conexion DB

---
+++ db/index.js
@@ -0,0 +1,41 @@
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: '192.168.100.89',
+  database: 'postgres',
+  password: 'c4123',
+  port:  5432,
+});
+
+module.exports = pool;
+
+//Pedir datos
+const obtenerCanales = async() => {
+  const res = await pool.query('SELECT * FROM public.canales');
+  console.log(res.rows);
+  pool.end(); //no para apps webs
+}
+//insertar datos
+const insertarCanal = async() => {
+  const text = 'INSERT INTO canales (nombre) VALUES ($1)';
+  const values = ['C'];
+
+  const res = await pool.query(text, values);
+  console.log(res);
+  pool.end();
+}
+
+//eliminar dato
+const borrarCanal = async () => {
+  const text = 'DELETE FROM canales WHERE nombre = $1';
+  const values = ['C'];
+  const res = await pool.query(text, values);
+  console.log(res);
+  pool.end();
+}
+
+//obtenerCanales();
+//insertarCanal();
+//borrarCanal();
